0dde16f68a196bdd954d741198ea8b42a79cd7d6
Vishnucreate <65901404+Vishnucreate@users.noreply.github.com>
2024-09-24 23:57:33 +0530

    Update ObjectDetection.java

--- ObjectDetection.java
+++ ObjectDetection.java
@@ -2,77 +2,83 @@ import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.services.rekognition.RekognitionClient;
 import software.amazon.awssdk.services.rekognition.model.*;
 import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
-import software.amazon.awssdk.services.sqs.model.SendMessageResponse;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Object;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
+import software.amazon.awssdk.services.sqs.model.Message;
 
-import java.io.InputStream;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class ObjectDetection {
-
-    private static final String BUCKET_NAME = "carimagestorage";
-    private static final String QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/323052225972/sqsforcarimage";
+    private static final String SQS_QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/323052225972/sqsforcarimage";  // Your SQS queue URL
+    private static final String S3_BUCKET_URL = "https://njit-cs-643.s3.us-east-1.amazonaws.com";  // Public S3 bucket URL
 
     public static void main(String[] args) {
-        S3Client s3Client = S3Client.builder().credentialsProvider(ProfileCredentialsProvider.create()).build();
-        RekognitionClient rekognitionClient = RekognitionClient.builder().credentialsProvider(ProfileCredentialsProvider.create()).build();
         SqsClient sqsClient = SqsClient.builder().credentialsProvider(ProfileCredentialsProvider.create()).build();
+        RekognitionClient rekognitionClient = RekognitionClient.builder().build();
 
-        // List images in the S3 bucket
-        ListObjectsV2Request listObjectsRequest = ListObjectsV2Request.builder().bucket(BUCKET_NAME).build();
-        ListObjectsV2Response listObjectsResponse = s3Client.listObjectsV2(listObjectsRequest);
+        boolean keepProcessing = true;
 
-        List<S3Object> images = listObjectsResponse.contents();
-        int index = 0;
-
-        for (S3Object image : images) {
-            String imageKey = image.key();
-
-            // Get image from S3
-            GetObjectRequest getObjectRequest = GetObjectRequest.builder().bucket(BUCKET_NAME).key(imageKey).build();
-            InputStream imageStream = s3Client.getObject(getObjectRequest);
-
-            // Call Rekognition for object detection
-            DetectLabelsRequest detectLabelsRequest = DetectLabelsRequest.builder()
-                    .image(Image.builder().s3Object(software.amazon.awssdk.services.rekognition.model.S3Object.builder().bucket(BUCKET_NAME).name(imageKey).build()).build())
-                    .maxLabels(10)
-                    .minConfidence(90F)
+        while (keepProcessing) {
+            // Receive messages from the SQS queue
+            ReceiveMessageRequest receiveRequest = ReceiveMessageRequest.builder()
+                    .queueUrl(SQS_QUEUE_URL)
+                    .maxNumberOfMessages(10)
+                    .waitTimeSeconds(20)
                     .build();
 
-            DetectLabelsResponse detectLabelsResponse = rekognitionClient.detectLabels(detectLabelsRequest);
+            ReceiveMessageResponse receiveResponse = sqsClient.receiveMessage(receiveRequest);
+            List<Message> messages = receiveResponse.messages();
 
-            // Check if a car is detected
-            boolean carDetected = detectLabelsResponse.labels().stream()
-                    .anyMatch(label -> label.name().equalsIgnoreCase("Car") && label.confidence() >= 90F);
+            for (Message message : messages) {
+                String imageIndex = message.body();
 
-            if (carDetected) {
-                System.out.println("Car detected in image: " + imageKey);
+                // Check for termination signal (-1)
+                if (imageIndex.equals("-1")) {
+                    System.out.println("No more images to process. Exiting...");
+                    keepProcessing = false;
+                    break;
+                }
 
-                // Send message to SQS
-                SendMessageRequest sendMessageRequest = SendMessageRequest.builder()
-                        .queueUrl(QUEUE_URL)
-                        .messageBody(imageKey)
-                        .build();
+                // Download the image from the public S3 bucket
+                String imageUrl = S3_BUCKET_URL + "/" + imageIndex + ".jpg";  // Assuming images are named as 1.jpg, 2.jpg, etc.
+                try {
+                    downloadImage(imageUrl, imageIndex + ".jpg");
 
-                SendMessageResponse sendMessageResponse = sqsClient.sendMessage(sendMessageRequest);
-                System.out.println("Message sent to SQS for image: " + imageKey);
+                    // Call Rekognition for object detection
+                    DetectLabelsRequest detectLabelsRequest = DetectLabelsRequest.builder()
+                            .image(Image.builder().s3Object(software.amazon.awssdk.services.rekognition.model.S3Object.builder()
+                                    .bucket("njit-cs-643")
+                                    .name(imageIndex + ".jpg")
+                                    .build()).build())
+                            .maxLabels(10)  // You can adjust the number of labels detected
+                            .minConfidence(75F)  // Adjust the confidence threshold
+                            .build();
+
+                    DetectLabelsResponse detectLabelsResponse = rekognitionClient.detectLabels(detectLabelsRequest);
+
+                    // Process the object detection results
+                    List<Label> labels = detectLabelsResponse.labels();
+                    System.out.println("Detected objects for image " + imageIndex + ".jpg:");
+                    for (Label label : labels) {
+                        System.out.println("Detected: " + label.name() + " (Confidence: " + label.confidence() + ")");
+                    }
+                } catch (IOException e) {
+                    System.err.println("Failed to download image: " + imageIndex + ".jpg");
+                    e.printStackTrace();
+                }
             }
-
-            index++;
         }
+    }
 
-        // Signal to Instance B that the processing is done
-        SendMessageRequest terminateMessageRequest = SendMessageRequest.builder()
-                .queueUrl(QUEUE_URL)
-                .messageBody("-1")  // Signal to Instance B
-                .build();
-
-        sqsClient.sendMessage(terminateMessageRequest);
-        System.out.println("Termination signal sent to SQS.");
+    // Method to download the image from the public S3 bucket URL
+    private static void downloadImage(String imageUrl, String outputFileName) throws IOException {
+        try (InputStream in = new URL(imageUrl).openStream()) {
+            Files.copy(in, Paths.get(outputFileName));
+            System.out.println("Image downloaded: " + outputFileName);
+        }
     }
 }
